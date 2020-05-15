3a78e43072f127318c942b8147a2801b8d13e714
Allan <xiaodoubizwx@gmail.com>
2020-05-15 01:35:26 -0700

    adjust the data structure of the return value of the extract_base function in base_structure.py

--- src/base_structure.py
+++ src/base_structure.py
@@ -7,8 +7,14 @@
 # Sn: special symbol  string of length n (contains only special symbols)
 # Dn: digit strings of length n (contains only digits)
 
+# Implementation:
+## Data Structure:
+# statsOfPw: {'base': <string>, 'digits': {{}}, 'symbols': {{}} }
+
 # Note: Special symbol is defined as non-digit and non-alphabet
 # TODO: add custom dictionary (terminals)
+
+
 
 import sys
 
@@ -16,6 +22,10 @@ def extract_base(line, charSet={}):
     
     l = s = d = 0
     base = ""
+    # dict of {digit streams: counter}
+    digits = {}
+    # dict of {symbol streams: counter}
+    symbols = {}
     digitStream = ""
     symbolStream = ""
     # record the stats of the pw
@@ -28,20 +38,20 @@ def extract_base(line, charSet={}):
             if s != 0: # the specical symbol stream has ended              
                 base += f"S{s}"                
                 # save the symbol stream and the counter of the stream 
-                if symbolStream in statsOfPw: 
-                    statsOfPw[symbolStream] += 1
+                if symbolStream in symbols: 
+                    symbols[symbolStream] += 1
                 else:
-                    statsOfPw[symbolStream] = 1
+                    symbols[symbolStream] = 1
                 # flush the temp variable to keep the invariant
                 s = 0 
                 symbolStream = ""
             elif d != 0: # the number stream has ended 
                 base += f"D{d}"  
                 # save the digit stream and the counter of the stream             
-                if digitStream in statsOfPw: 
-                    statsOfPw[digitStream] += 1
+                if digitStream in digits: 
+                    digits[digitStream] += 1
                 else:
-                    statsOfPw[digitStream] = 1
+                    digits[digitStream] = 1
                 # flush the temp variable
                 d = 0
                 digitStream = ""
@@ -53,10 +63,10 @@ def extract_base(line, charSet={}):
                 l = 0
             elif s != 0: # the specical symbol stream has ended 
                 base += f"S{s}"
-                if symbolStream in statsOfPw: 
-                    statsOfPw[symbolStream] += 1
+                if symbolStream in symbols: 
+                    symbols[symbolStream] += 1
                 else:
-                    statsOfPw[symbolStream] = 1
+                    symbols[symbolStream] = 1
                 s = 0
                 symbolStream  = ""
             d += 1
@@ -68,18 +78,18 @@ def extract_base(line, charSet={}):
                 l = 0
             elif s != 0: # the specical symbol stream has ended 
                 base += f"S{s}"            
-                if symbolStream in statsOfPw: 
-                    statsOfPw[symbolStream] += 1
+                if symbolStream in symbols: 
+                    symbols[symbolStream] += 1
                 else:
-                    statsOfPw[symbolStream] = 1
+                    symbols[symbolStream] = 1
                 s = 0
                 symbolStream  = ""
             elif d != 0:
                 base += f"D{d}"
-                if digitStream in statsOfPw: 
-                    statsOfPw[digitStream] += 1
+                if digitStream in digits: 
+                    digits[digitStream] += 1
                 else:
-                    statsOfPw[digitStream] = 1
+                    digits[digitStream] = 1
                 d = 0
                 digitStream = ""
             else: # this line only contains the new line char
@@ -91,16 +101,18 @@ def extract_base(line, charSet={}):
                 l = 0
             elif d != 0: # the number stream has ended 
                 base += f"D{d}"
-                if digitStream in statsOfPw: 
-                    statsOfPw[digitStream] += 1
+                if digitStream in digits: 
+                    digits[digitStream] += 1
                 else:
-                    statsOfPw[digitStream] = 1
+                    digits[digitStream] = 1
                 d = 0
                 digitStream = ""
             s += 1
             symbolStream += c
     
     statsOfPw["base"] = base
+    statsOfPw["digits"] = digits
+    statsOfPw["symbols"] = symbols
     return statsOfPw
 
 def main():
